b1780e78f2a02f94f313848d46baaabb812efe26
Rosie Choi <rosiechoimanso@gmail.com>
2021-04-01 19:51:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,11 +2,12 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-if Time.now.hour - 12<= 0
+  if Time.now.hour - 12<= 0
     resp.write "Good Morning!"
   else
     resp.write "Good Afternoon!"
   end
     resp.finish
   end
+
 end
